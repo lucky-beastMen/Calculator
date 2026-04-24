9a3951f4a88fe8cf3e2c972810a8de5983947559
beastieboi504 <beastieboi504@gmail.com>
2026-04-24 15:10:34 +0300

    Version 1.3 Fixed the multiple operator bug and added the clear functionality

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ const operators = document.querySelector('.operators');
 const equalSign = document.querySelector('.equals');
 let currentNumber = '';
 let operatorClicked = false;
-let equalSignClicked = false;
 let currentDisplayedNumber = ''
-let a = 0;
+let a = display.textContent;
 let b = 0;
 let operator = '';
 numbers.addEventListener('click', (e) => {
@@ -20,7 +19,6 @@ numbers.addEventListener('click', (e) => {
             currentNumber = (currentNumber || '') + (currentDisplayedNumber);
             display.textContent = display.textContent + currentDisplayedNumber;
             b = currentNumber;
-            console.log(b);
             
         }
         
@@ -29,48 +27,47 @@ numbers.addEventListener('click', (e) => {
 })
 
 operators.addEventListener('click', (e) => {
-    if(e.target.textContent !== "=" && display.textContent.includes(e.target.textContent) === false){
+    
+    if(e.target.textContent !== "=" && e.target.textContent !== 'Clear' && operatorClicked === false){
         operatorClicked = true;
         display.textContent = display.textContent + " " + e.target.textContent;
         a = currentNumber;
         operator = e.target.textContent;
         currentNumber = '';
-    }else if(display.textContent.includes(e.target.textContent ) && e.target.textContent !== "=" ){
+    }else if(operatorClicked === true && e.target.textContent !== "=" && e.target.textContent !== 'Clear' ){
+
         operatorClicked = true;
         display.textContent = display.textContent + e.target.textContent;
         a = operatorFun(operator, Number(a), Number(b));
+        operator = e.target.textContent;
+        
         b = 0;
-        console.log(a, b);
-        currentNumber = ''
+        
+        currentNumber = '';
+    }else if(e.target.textContent === "="){
+        
+        display.textContent = operatorFun(operator, Number(a), Number(b));
+    }else if(e.target.textContent == "Clear"){
+        a = display.textContent;
+        b = 0;
+        display.textContent = 0;
+        currentNumber = '';
+        currentDisplayedNumber = '';
+        operatorClicked = false;
     }
 
 })
-equalSign.addEventListener('click', (e) => {
-    equalSignClicked = true;
-    display.textContent = operatorFun(operator, Number(a), Number(b));
-})
 
-function add(a, b){
-    return a + b;
-}
-function subtract(a, b){
-    return a - b;
-}
-function multiply(a, b){
-    return a * b;
-}
-function divide(a, b){
-    return a/b;
-}
+
 function operatorFun(opp, a, b){
     if(opp == "+"){
-        return add(a, b)
+        return a + b;
     }else if(opp == "-"){
-        return subtract(a, b)
+        return a - b;
     }else if(opp == "*"){
-        return multiply(a, b)
+        return a * b;
     }else if(opp == "/"){
-        return divide(a, b)
+        return (a/b).toFixed(4);
     }else{
         return "Something went wrong";
     }
--- test.js
+++ test.js
@@ -1,2 +1,14 @@
-let a = 'kaleb';
-console.log(a.includes('x'));
+if(e.target.textContent !== "=" && display.textContent.includes(e.target.textContent) === false){
+        operatorClicked = true;
+        display.textContent = display.textContent + " " + e.target.textContent;
+        a = currentNumber;
+        operator = e.target.textContent;
+        currentNumber = '';
+    }else if(display.textContent.includes(e.target.textContent ) && e.target.textContent !== "=" ){
+        operatorClicked = true;
+        display.textContent = display.textContent + e.target.textContent;
+        a = operatorFun(operator, Number(a), Number(b));
+        b = 0;
+        console.log(a, b);
+        currentNumber = ''
+    }
