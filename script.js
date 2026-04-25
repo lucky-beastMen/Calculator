4814f0563ec92071af93bb7193f2d7d303d53ec2
beastieboi504 <beastieboi504@gmail.com>
2026-04-26 01:40:49 +0300

    Version 1.3.4 Added a dot

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ let operatorClicked = false;
 let currentDisplayedNumber = ''
 let a = display.textContent;
 let b = 0;
+let dotClicked = false;
 let operator = '';
 let lastInput = '';
 numbers.addEventListener('click', (e) => {
     if(e.target.classList.contains('number')){
+        if(e.target.textContent == '.'){
+            
+        }
         if(display.textContent === "Can't divide by 0 :))"){
             display.textContent == '';
             a = display.textContent;
@@ -30,18 +34,31 @@ numbers.addEventListener('click', (e) => {
         }
         if(operatorClicked !== true){
             currentNumber = currentNumber + e.target.textContent;
+            
             display.textContent = currentNumber;
+            
             lastInput = 'number';
         }
         else if(operatorClicked === true){
             currentDisplayedNumber = e.target.textContent;
             currentNumber = (currentNumber || '') + (currentDisplayedNumber);
-            display.textContent = display.textContent + currentDisplayedNumber;
+            if(lastInput == 'number'){
+                display.textContent = display.textContent + currentDisplayedNumber;
+            }else if(lastInput != 'number'){
+                display.textContent = display.textContent + ' ' +currentDisplayedNumber;
+            }
             b = currentNumber;
+            
+            
             lastInput = 'number';
         }
         
-    } 
+        
+    }else if(e.target.classList.contains('dot') && dotClicked == false){
+        display.textContent = display.textContent + e.target.textContent;
+        currentNumber = currentNumber + '.';
+        dotClicked = true;
+    }
 
 })
 
@@ -66,9 +83,16 @@ operators.addEventListener('click', (e) => {
             operator = e.target.textContent;
             currentNumber = '';
             lastInput = 'operator';
+            dotClicked = false;
         }else if(operatorClicked === true && e.target.textContent !== "=" && e.target.textContent !== 'Clear' ){
             operatorClicked = true;
-            display.textContent = display.textContent + e.target.textContent;
+            dotClicked = false;
+            if(lastInput == 'equals'){
+                display.textContent = display.textContent + " " +e.target.textContent;
+            }
+            else{
+                display.textContent = display.textContent + e.target.textContent;
+            }
             a = operatorFun(operator, Number(a), Number(b));
             operator = e.target.textContent;
             b = 0;
@@ -77,6 +101,7 @@ operators.addEventListener('click', (e) => {
         }else if(e.target.textContent === "="){
             display.textContent = operatorFun(operator, Number(a), Number(b));
             lastInput = 'equals';
+            dotClicked = false;
         }else if(e.target.textContent == "Clear"){
             a = display.textContent;
             b = 0;
@@ -84,7 +109,7 @@ operators.addEventListener('click', (e) => {
             currentNumber = '';
             currentDisplayedNumber = '';
             operatorClicked = false;
-
+            dotClicked = false;
         }
     }
     
